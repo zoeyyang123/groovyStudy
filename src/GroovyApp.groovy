6d6374b2c1a8c40b0fcaafa617b68596aa9b2029
zhaoyi <zoeyyang_bupt@163.com>
2017-06-07 16:38:07 +0800

    开始学groovy，这是一个helloworld

---
+++ src/GroovyApp.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by zhaoyi on 17-6-7.
+ */
+class GroovyApp {
+    void hello(){
+        println("Hello,world!");
+    }
+}
---
+++ src/main.groovy
@@ -0,0 +1,5 @@
+/**
+ * Created by zhaoyi on 17-6-7.
+ */
+app = new GroovyApp()
+app.hello()
